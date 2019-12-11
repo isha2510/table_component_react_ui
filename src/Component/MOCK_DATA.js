ea67d2882e836b96ad9b1322f39732e8a9e43992
isha2510 <ishatripathi80@gmail.com>
2019-12-11 23:17:11 +0530

    mock data

---
+++ src/Component/MOCK_DATA.js
@@ -0,0 +1,13 @@
+const data=[
+{"id":1,"name":"Abraham Fabri","email":"afabri0@scientificamerican.com","address":"9065 Becker Way"},
+{"id":2,"name":"Wain Marfell","email":"wmarfell1@ft.com","address":"371 Del Mar Circle"},
+{"id":3,"name":"Elwyn Matthaus","email":"ematthaus2@jugem.jp","address":"345 Michigan Center"},
+{"id":4,"name":"Bernardo Attwater","email":"battwater3@stanford.edu","address":"33690 Melvin Trail"},
+{"id":5,"name":"Bay Dalgliesh","email":"bdalgliesh4@cbsnews.com","address":"5 Blue Bill Park Park"},
+{"id":6,"name":"Nancee Curry","email":"ncurry5@goodreads.com","address":"725 Kingsford Plaza"},
+{"id":7,"name":"Marlow Gentry","email":"mgentry6@istockphoto.com","address":"87717 Weeping Birch Pass"},
+{"id":8,"name":"Kelby Eouzan","email":"keouzan7@php.net","address":"1 Kim Pass"},
+{"id":9,"name":"Dorine Dickin","email":"ddickin8@seattletimes.com","address":"51 Johnson Crossing"},
+{"id":10,"name":"Yolanda Guwer","email":"yguwer9@dailymotion.com","address":"0 Northridge Circle"}]
+
+export default data;
